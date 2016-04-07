1617d83eab78be65cdbb6cd1e10c4b3aacf932fa
Pedro Chinen <chinen93@gmail.com>
2016-04-07 15:07:27 -0300

    Update pchinen

--- files/pchinen.el
+++ files/pchinen.el
@@ -79,6 +79,7 @@
     (message "Emacs is NOT up, so i started it")))
 
 ;; Load theme
+(use-package zenburn-theme :ensure t)
 (use-package monokai-theme
   :ensure t
   :config
@@ -86,6 +87,15 @@
     (message "Monokai Theme - Loaded")
     (load-theme 'monokai t)
     (set-background-color "#121212")))
+
+(defun my/change-theme()
+  (interactive)
+  (message "Theme Changed")
+  )
+
+(when window-system 
+  (my/change-theme)
+  )
 
 (use-package company
   :ensure t
