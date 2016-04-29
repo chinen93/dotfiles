8c7f06ab9353540e2910a948962677c75e6b732a
Pedro Chinen <chinen93@gmail.com>
2016-04-28 22:26:10 -0300

    Python settings put into a python-mode hook

--- files/emacsConfig/init-hooks.el
+++ files/emacsConfig/init-hooks.el
@@ -2,6 +2,7 @@
 ;; Hooks
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; org-mode
 (add-hook 'org-mode-hook
 	  ;; Create hook when org mode is enabled
 	  (lambda()
@@ -9,14 +10,25 @@
 	    ))
 
 
-;; use the python 3.1
-(setq py-python-command "/usr/bin/python3.1")
-(when (fboundp 'electric-indent-mode) (electric-indent-mode -1))
 
-(use-package company-jedi
-  :ensure t
-  :config (progn 
-	    (add-to-list 'company-backends 'company-jedi)))
+;; python-mode
+(add-hook 'python-hook
+	  (progn 
+	    ;; use the python 3.1
+	    (setq py-python-command "/usr/bin/python3.1")
+	    (when (fboundp 'electric-indent-mode) (electric-indent-mode -1))
+	    
+	    (use-package company-jedi
+	      :ensure t
+	      :config (progn 
+			(add-to-list 'company-backends 'company-jedi)))
+
+	    ;; progn
+	    )
+
+	  ;; add-hook 'python-mode
+	  )
+
+
 (provide 'init-hooks)
-
 
