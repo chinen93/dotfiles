e6163d5f95f8450449bfc1ffb36308fdaaffe994
Pedro Chinen <chinen93@gmail.com>
2019-11-12 11:31:51 -0300

    Clean up all the unused files and scripts

--- bin/shellscript/checkInstalledPrograms.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# _version_: 0.1
-# __author_: Pedro Chinen
-# ____date_: 2016-04-07
-
-# Description:
-# Script to show if all the programs that i use are installed
-
-
--- initSystem/startPrograms.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-##
-##  Programs to execute when system is loaded
-##
-firefox &
-thunderbird &
-emacs &
